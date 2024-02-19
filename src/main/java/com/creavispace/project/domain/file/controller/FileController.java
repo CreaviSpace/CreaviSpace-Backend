b2e68d74b5536c47b85787a2392c7af3231328d9
YuHoSeong <ghtjd026@gmail.com>
2024-02-19 17:53:53 +0900

    feat(File) : File 도메인 생성 rename : File 관련 controller, service, entity 이동 / S3UploadService -> FileServiceImpl 이름 변경 feat(FileService) : FileService interface 생성 feat(UploadFileResponseDto) : 파일 업로드 ResponseDto 생성 feat(SecurityConfig) : 파일 업로드 설정 추가 refactor(FileServiceImpl) : 반환 타입 변경 String -> UploadFileResponseDto refactor(FileController) : fileUpload에 PostMapping MediaType 추가
     #47

--- src/main/java/com/creavispace/project/domain/file/controller/FileController.java
+++ src/main/java/com/creavispace/project/domain/file/controller/FileController.java
@@ -1,13 +1,15 @@
-package com.creavispace.project.domain.common.controller;
+package com.creavispace.project.domain.file.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.creavispace.project.domain.common.dto.SuccessResponseDto;
-import com.creavispace.project.domain.common.service.S3UploadService;
+import com.creavispace.project.domain.file.dto.response.UploadFileResponseDto;
+import com.creavispace.project.domain.file.service.S3UploadService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +21,8 @@ public class FileController {
     
     private final S3UploadService s3UploadService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<SuccessResponseDto<String>> fileUpload(@RequestParam(value = "file", required = true) MultipartFile file) throws Exception {
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SuccessResponseDto<UploadFileResponseDto>> fileUpload(@RequestParam(value = "file", required = true) MultipartFile file) throws Exception {
         return ResponseEntity.ok().body(s3UploadService.saveFile(file));
     }
 
